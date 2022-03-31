9c97dd68bb9b84d91b477ef154739cec29b6843b
Lucas Eiji <lucasaccessor@hotmail.com>
2022-03-31 10:11:54 -0300

    ex02 adicionado

---
+++ src/espm/poo/ex2/Banco.java
@@ -0,0 +1,39 @@
+package espm.poo.ex2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Banco {
+
+    private String nome;
+
+    private List<Cliente> clientes;
+    private List<Conta> contas;
+
+    public Banco(String nome) {
+        this.nome = nome;
+        this.clientes = new ArrayList<>();
+        this.setContas(new ArrayList<>());
+    }
+
+    public List<Conta> getContas() {
+        return contas;
+    }
+
+    public void setContas(List<Conta> contas) {
+        this.contas = contas;
+    }
+
+    public void addCliente(Cliente c) {
+        this.clientes.add(c);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public List<Cliente> getClientes() {
+        return clientes;
+    }
+    
+}
---
+++ src/espm/poo/ex2/Conta.java
@@ -0,0 +1,18 @@
+package espm.poo.ex2;
+
+public class Conta {
+
+    private int id;
+    private double saldo;
+
+
+}
+
+
+public double getSaldo(){
+    return saldo;
+}
+
+public void sacar(double valor){
+    
+}
